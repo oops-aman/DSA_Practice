a2ed3883565572edfab1b4b3de086276c4ccdda7
oops-aman <b200054@nitsikkim.ac.in>
2022-05-17 13:02:49 +0530

    Reverse an array

--- rev_array.cpp
+++ rev_array.cpp
@@ -1,26 +1,36 @@
-//Problem: Reversing an array
+// Problem: Reversing an array
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int n, i, num;
+    int i;
+    int n;
     cout << "Enter number of elements : ";
     cin >> n;
-    vector<int> arr(n), revArr(n);
-    cout << "Enter array elements : ";
-    for(i = 0; i < n; i++) {
+    vector<int> arr(n);
+
+    cout << "Enter array elements : " << endl;
+    for (i = 0; i < n; i++)
+    {
         cin >> arr[i];
     }
-    int k = n-1;
-    for(i = 0; i < n; i++) {
-        revArr[i] = arr[k];
-        k--;
+
+    int first = 0, last = n - 1;
+    while (first < last)
+    {
+        int temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
+        first++;
+        last--;
     }
-    cout << "Array after reversing : ";
-    for(auto i: revArr) 
+
+    cout << "Array after reversing : " << endl;
+    for(auto i: arr) {
         cout << i << " ";
+    }
 
     return 0;
 }
